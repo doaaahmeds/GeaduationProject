4cd347f047fdd1d456949da21955cd3672c10894
Hassan <hassanesam308@gmail.com>
2023-02-26 11:54:16 +0200

    modify iproduct

--- src/app/Products-module/prod/products/products.component.ts
+++ src/app/Products-module/prod/products/products.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
+  
 
 }
---
+++ src/app/models/products.ts
@@ -0,0 +1,15 @@
+export interface Products {
+    id:string,
+    catid: string,
+    subid: string,
+    name: string,
+    details: string[],
+    size: Map<string, number>[],
+    colors: Map<string, number>[],
+    imgs: string[],
+    offer: boolean,
+    old_price: number,
+    new_price: number,
+    discount: number,
+
+}
