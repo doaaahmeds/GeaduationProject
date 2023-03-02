409357c7a5b1f3056662d6385b91d395be69dd0b
doaaahmeds <76729277+doaaahmeds@users.noreply.github.com>
2023-03-03 00:34:04 +0200

    edit

--- src/app/Products-module/prod/products/products.component.ts
+++ src/app/Products-module/prod/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Iproduct } from 'src/app/models/iproduct';
 import { ProductsAPIService } from 'src/app/services/products-api.service';
 
@@ -11,15 +12,18 @@ export class ProductsComponent implements OnInit {
 
   products: Iproduct[] = []
 
-  constructor(private prodAPIService: ProductsAPIService) { }
+  constructor(private prodAPIService: ProductsAPIService,private route:ActivatedRoute) { }
+
+  
 
   ngOnInit(): void {
     this.prodAPIService.getProductesOfSub().subscribe(data => {
-
       this.products = data
-      // console.log(this.products);
-      
+      // console.log(this.products);      
     })
+
+
+
   }
 
 
--- src/app/services/products-api.service.ts
+++ src/app/services/products-api.service.ts
@@ -50,6 +50,7 @@ export class ProductsAPIService {
     return collectionData(q, { idField: 'id' }) as Observable<Iproduct[]>
   }
 
+  
 
   // get all products
   getAllProductes(): Observable<Iproduct[]> {
