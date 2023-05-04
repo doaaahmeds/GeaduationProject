9dec5d31bb0c87a688b3d4b4d9f9ad86bc4f398e
Hassan <hassanesam308@gmail.com>
2023-05-04 10:13:23 +0300

    origin firstore

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,19 +1,19 @@
 export const environment = {
-  // firebase: {
-  //   projectId: 'dejavu-65fe7',
-  //   appId: '1:766943688283:web:98f86064843e383503b17e',
-  //   storageBucket: 'dejavu-65fe7.appspot.com',
-  //   apiKey: 'AIzaSyCWGJcIQWQ86GxANkx_QvkFJknOnHShPXM',
-  //   authDomain: 'dejavu-65fe7.firebaseapp.com',
-  //   messagingSenderId: '766943688283',
-  // },
   firebase: {
-    projectId: "dejavu1-c6625",
-    appId: "1:189380682158:web:866cfb30c4325d0208a83f",
-    storageBucket: "dejavu1-c6625.appspot.com",
-    apiKey: "AIzaSyAgHnePKMwNjLTgiTvCBE7rvhqGqi9LYM0",
-    authDomain: "dejavu1-c6625.firebaseapp.com",
-    messagingSenderId: "189380682158", 
+    projectId: 'dejavu-65fe7',
+    appId: '1:766943688283:web:98f86064843e383503b17e',
+    storageBucket: 'dejavu-65fe7.appspot.com',
+    apiKey: 'AIzaSyCWGJcIQWQ86GxANkx_QvkFJknOnHShPXM',
+    authDomain: 'dejavu-65fe7.firebaseapp.com',
+    messagingSenderId: '766943688283',
   },
+  // firebase: {
+  //   projectId: "dejavu1-c6625",
+  //   appId: "1:189380682158:web:866cfb30c4325d0208a83f",
+  //   storageBucket: "dejavu1-c6625.appspot.com",
+  //   apiKey: "AIzaSyAgHnePKMwNjLTgiTvCBE7rvhqGqi9LYM0",
+  //   authDomain: "dejavu1-c6625.firebaseapp.com",
+  //   messagingSenderId: "189380682158", 
+  // },
   clientId:'AT-DPLdQtvkIIjSS71lIqhCB4bE01cXXN2OaFCX07OHYNF4payxXUzJ_TVxtb-GoPilx0Cp3ahC9Ak3A'
 };
